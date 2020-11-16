57767052e746890879b397198ce5545470605c6d
J1NC <34640231+J1NC@users.noreply.github.com>
2020-11-16 15:22:19 +0900

    Initialize Server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
